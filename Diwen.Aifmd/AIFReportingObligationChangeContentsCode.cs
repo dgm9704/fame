f3bb6bf16cc7d3c20fa2990e3d3b341d3b4ea3e3
dgm9704 <dgm9704@hotmail.com>
2018-03-18 23:09:40 +0200

    descriptions

--- Diwen.Aifmd/AIFReportingObligationChangeContentsCode.cs
+++ Diwen.Aifmd/AIFReportingObligationChangeContentsCode.cs
@@ -22,84 +22,111 @@
 namespace Diwen.Aifmd
 {
     using System;
+    using System.ComponentModel;
     using System.Xml.Serialization;
+    
     [Serializable]
     public enum AIFReportingObligationChangeContentsCode
     {
 
         [XmlEnum("1")]
+        [Description("'24(1)+(2)+(4)' to '24(1)+(2)'")]
         Item1,
 
         [XmlEnum("2")]
+        [Description("'24(1)+(2)+(4)' to '24(1)'")]
         Item2,
 
         [XmlEnum("3")]
+        [Description("'24(1)+(2)+(4)' to '3(3d)'")]
         Item3,
 
         [XmlEnum("4")]
+        [Description("'24(1)+(2)+(4)' to '24(1)+(4)'")]
         Item4,
 
         [XmlEnum("5")]
+        [Description("'24(1)+(2)' to '24(1)+(2)+(4)'")]
         Item5,
 
         [XmlEnum("6")]
+        [Description("'24(1)+(2)' to '24(1)'")]
         Item6,
 
         [XmlEnum("7")]
+        [Description("'24(1)+(2)' to '3(3d)'")]
         Item7,
 
         [XmlEnum("8")]
+        [Description("'24(1)+(2)' to '4(1)+(4)'")]
         Item8,
 
         [XmlEnum("9")]
+        [Description("'24(1)' to '24(1)+(2)+(4)'")]
         Item9,
 
         [XmlEnum("10")]
+        [Description("'24(1)' to '24(1)+(2)'")]
         Item10,
 
         [XmlEnum("11")]
+        [Description("'24(1)' to '3(3d)'")]
         Item11,
 
         [XmlEnum("12")]
+        [Description("'24(1)' to '24(1)+(4)'")]
         Item12,
 
         [XmlEnum("13")]
+        [Description("'3(3d)' to '24(1)+(2)+(4)'")]
         Item13,
 
         [XmlEnum("14")]
+        [Description("'3(3d)' to '24(1)+(2)'")]
         Item14,
 
         [XmlEnum("15")]
+        [Description("'3(3d)' to '24(1)'")]
         Item15,
 
         [XmlEnum("16")]
+        [Description("'3(3d)' to '24(1)+(4)'")]
         Item16,
 
         [XmlEnum("17")]
+        [Description("'24(1)+(4)' to '24(1)+(2)+(4)'")]
         Item17,
 
         [XmlEnum("18")]
+        [Description("'24(1)+(4)' to '24(1)+(2)'")]
         Item18,
 
         [XmlEnum("19")]
+        [Description("'24(1)+(4)' to '24(1)'")]
         Item19,
 
         [XmlEnum("20")]
+        [Description("'24(1)+(4)' to '3(3d)'")]
         Item20,
 
         [XmlEnum("21")]
+        [Description("'N/A' to '24(1)+(2)+(4)'")]
         Item21,
 
         [XmlEnum("22")]
+        [Description("'N/A' to '24(1)+(2)'")]
         Item22,
 
         [XmlEnum("23")]
+        [Description("'N/A' to '24(1)'")]
         Item23,
 
         [XmlEnum("24")]
+        [Description("'N/A' to '3(3d)'")]
         Item24,
 
         [XmlEnum("25")]
+        [Description("'N/A' to '24(1)+(4)'")]
         Item25,
     }
 }
