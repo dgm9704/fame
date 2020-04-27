ee2eb4e268e1cda18cffe573fb562f1d84627528
John Nordberg <john.nordberg@gmail.com>
2020-04-28 01:17:21 +0300

    More input/output functionality, cleanup, remove .NET Framework 4.5.2

--- Diwen.Aifmd.Test/SampleTests.cs
+++ Diwen.Aifmd.Test/SampleTests.cs
@@ -5,23 +5,126 @@ namespace Diwen.Aifmd.Test
 {
     using Xunit;
     using Diwen.Aifmd;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using Xunit.Abstractions;
+    using System.IO;
+    using System.Linq;
+    using System.Xml;
+    using System;
+    using System.Collections.Generic;
 
-    public class Sampletests
+    public class SampleTests
     {
+        private readonly ITestOutputHelper output;
+
+        public SampleTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void ExportManager()
-            => new AIFMReportingInfo().ToFile("manager_out.xml");
+            => new AIFMReportingInfo().ToFile("report/manager_out.xml");
 
         [Fact]
         public void ImportManager()
-            => AIFMReportingInfo.FromFile("AIFMSample.xml");
+            => AIFMReportingInfo.FromFile("report/AIFMSample.xml");
 
         [Fact]
         public void ExportFund()
-            => new AIFReportingInfo().ToFile("fund_out.xml");
+            => new AIFReportingInfo().ToFile("report/fund_out.xml");
 
         [Fact]
         public void ImportFund()
-            => AIFReportingInfo.FromFile("AIFSample.xml");
+            => AIFReportingInfo.FromFile("report/AIFSample.xml");
+
+        [Fact]
+        public void ImportIncorrect()
+        {
+            var a0 = AIFReportingInfo.FromFile("report/incorrect.xml");
+            var a1 = XDocument.Load("report/incorrect.xml");
+
+            var schemas = GetSchemas();
+
+            a1.Validate(schemas, (o, e) => ValidationEventHandler(o, e));
+
+            a0.ToFile("report/correct.xml");
+
+            var a2 = XDocument.Load("report/correct.xml");
+
+            a2.Validate(schemas, (o, e) => ValidationEventHandler(o, e));
+
+        }
+
+
+        private static XmlSchemaSet GetSchemas()
+        {
+            var schemas = new XmlSchemaSet();
+            Directory.GetFiles("schema", "*.xsd").ToList().ForEach(s => schemas.Add(null, s));
+            schemas.Compile();
+            return schemas;
+        }
+
+
+        [Fact]
+        public void ImportFoo()
+        {
+
+            var settings = new XmlReaderSettings();
+            settings.Schemas.Add("", "schema/AIFMD_REPORTING_DataTypes_V1.2.xsd");
+            settings.Schemas.Add("", "schema/AIFMD_DATAIF_V1.2.xsd");
+            settings.ValidationType = ValidationType.Schema;
+
+            var reader = XmlReader.Create("correct.xml", settings);
+            var document = new XmlDocument();
+            document.Load(reader);
+
+            var eventHandler = new ValidationEventHandler(ValidationEventHandler);
+
+            // the following call to Validate succeeds.
+            document.Validate(eventHandler);
+
+        }
+
+        void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            var message = $"{e.Severity}: {e.Message}";
+            output.WriteLine(message);
+            Console.WriteLine(message);
+        }
+
+        [Fact]
+        public void ExportReport()
+        {
+            var report = Aifmd.AIFReportingInfo.FromFile("report/aifsample.xml");
+            var records = report.GetRecords();
+            WriteRecords(records, "output/aifsample.txt");
+        }
+
+        [Fact]
+        public void ImportReport()
+        {
+            var data =
+                File.ReadAllLines("report/import_shuffle.txt").
+                Select(l => l.Split(";")).
+                ToDictionary(
+                    l => l.First(),
+                    l => l.Last());
+
+            var document = AIFReportingInfo.WriteReport(data);
+            Directory.CreateDirectory("output");
+            document.Save("output/import_shuffle.xml");
+
+            var report = AIFReportingInfo.FromFile("output/import_shuffle.xml");
+            report.ToFile("output/corrected.xml");
+
+        }
+
+        private void WriteRecords(Dictionary<string, string> records, string path)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            File.WriteAllLines(path, records.Select(r => $"{r.Key};{r.Value}"));
+        }
     }
 }
--- Diwen.Aifmd/AIFReportingInfo.Serialization.cs
+++ Diwen.Aifmd/AIFReportingInfo.Serialization.cs
@@ -22,11 +22,22 @@
 namespace Diwen.Aifmd
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Xml;
+    using System.Xml.Linq;
     using System.Xml.Serialization;
+    using System.Xml.XPath;
 
     public partial class AIFReportingInfo
     {
+
+        static AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+        static Version version = assembly.Version;
+        static string id = assembly.Name;
+
         private static Lazy<XmlSerializer> Serializer = new Lazy<XmlSerializer>(()
             => new XmlSerializer(typeof(AIFReportingInfo)));
 
@@ -44,5 +55,126 @@ namespace Diwen.Aifmd
             using (var file = new FileStream(path, FileMode.Create))
                 Serializer.Value.Serialize(file, report);
         }
+
+        public Dictionary<string, string> GetRecords()
+        {
+            var document = this.ToXDocument();
+            var result = new Dictionary<string, string>();
+            result =
+                document.
+                Descendants().
+                Where(d => !d.Descendants().Any()).
+                ToDictionary(
+                    l => GetPath(l),
+                    l => l.Value);
+
+            return result;
+        }
+
+        public static XDocument WriteReport(Dictionary<string, string> data)
+        {
+            var report = new XDocument();
+            foreach (var item in data)
+            {
+                var node = ElementFromPath(report, item.Key);
+                node.Value = item.Value;
+            }
+            return report;
+        }
+
+        public static XElement ElementFromPath(XDocument document, string path)
+        {
+            if (document == null)
+                throw new ArgumentNullException("document");
+
+            if (path == null)
+                throw new ArgumentNullException("path");
+
+            var parts = path.Split('.');
+
+            var node = document.XPathSelectElement(parts[0]);
+            if (node == null)
+            {
+                document.Add(new XElement(parts[0]));
+                node = document.XPathSelectElement(parts[0]);
+            }
+
+            for (int i = 1; i < parts.Length; i++)
+            {
+                var part = parts[i];
+                var next = node.XPathSelectElement(part);
+
+                if (next == null)
+                {
+                    var x = part.IndexOf('[');
+                    if (x != -1)
+                    {
+                        var p = part.Remove(x);
+                        do
+                        {
+                            node.Add(new XElement(p));
+                            next = node.XPathSelectElement(part);
+                        } while (next == null);
+                    }
+                    else
+                    {
+                        node.Add(new XElement(part));
+                    }
+
+                    next = node.XPathSelectElement(part);
+                }
+                node = next;
+            }
+            return node;
+        }
+
+        public static string GetPath(XElement element)
+        {
+            if (element == null)
+                throw new ArgumentNullException("element");
+
+            var path = element.Name.LocalName;
+
+            if (element.Parent != null)
+            {
+                int idx;
+                var siblings = element.Parent.Elements(element.Name).ToList();
+                if (siblings.Count != 1)
+                {
+                    idx = siblings.IndexOf(element) + 1;
+                    if (idx != 0)
+                        path += "[" + idx + "]";
+                }
+                path = GetPath(element.Parent) + "." + path;
+            }
+            return path;
+        }
+
+        public XDocument ToXDocument()
+        => XDocument.Parse(this.ToXml());
+
+        void ToXmlWriter(XmlWriter writer)
+        {
+            var ns = new XmlSerializerNamespaces();
+            var info = $" {id} {version} ";
+            Serializer.Value.Serialize(writer, this, ns);
+            writer.WriteComment(info);
+        }
+
+        public XmlDocument ToXmlDocument()
+        {
+            var document = new XmlDocument();
+            var declaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);
+            document.AppendChild(declaration);
+            var nav = document.CreateNavigator();
+
+            using (var writer = nav.AppendChild())
+                ToXmlWriter(writer);
+
+            return document;
+        }
+
+        public string ToXml()
+        => ToXmlDocument().OuterXml;
     }
 }
